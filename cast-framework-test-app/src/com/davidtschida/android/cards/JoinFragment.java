9f6e457943352b3f4c555f43519ecc4dfae84562
jkoncel <joekoncel@yahoo.com>
2014-09-17 22:03:42 -0400

    JoinFragment sends join command, receives command and parses command it expects

--- cast-framework-test-app/src/com/davidtschida/android/cards/JoinFragment.java
+++ cast-framework-test-app/src/com/davidtschida/android/cards/JoinFragment.java
@@ -9,8 +9,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.davidtschida.android.cast.framework.OnCastConnectedListener;
@@ -23,9 +21,8 @@ import org.json.JSONObject;
  * Created by Joe Koncel on 9/17/2014.
  */
 public class JoinFragment extends CastFragment implements OnMessageReceivedListener, OnCastConnectedListener {
-    MediaRouteButton sendButton;
-    TextView receive;
-    //MediaRouteButton mrb;
+    Button sendButton;
+    MediaRouteButton mrb;
 
     public JoinFragment() {
     }
@@ -33,20 +30,20 @@ public class JoinFragment extends CastFragment implements OnMessageReceivedListe
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.join_fragment, container, false);
-        sendButton = (MediaRouteButton) rootView.findViewById(R.id.media_route_button);
-        host.getCastmanager().setMediaRouteActionProvider(sendButton);
+        mrb = (MediaRouteButton) rootView.findViewById(R.id.media_route_button);
+        host.getCastmanager().setMediaRouteActionProvider(mrb);
+        sendButton = (Button) rootView.findViewById(R.id.join_button);
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try {
-                    JSONObject o = new JSONObject("");
+                    JSONObject o = new JSONObject("{ \"command\": \"join\" }");
                     host.getCastmanager().sendMessage(o);
                 } catch(JSONException e) {
                     e.printStackTrace();
                 }
             }
         });
-        receive = (TextView) rootView.findViewById(R.id.receive);
         return rootView;
     }
 
@@ -59,12 +56,37 @@ public class JoinFragment extends CastFragment implements OnMessageReceivedListe
 
     @Override
     public void onMessageRecieved(JSONObject json) {
-        try {
-            Toast.makeText(getActivity(), "MOOO "+json.toString(4), Toast.LENGTH_LONG).show();
-            receive.setText(json.toString(4));
-        } catch(JSONException e) {
-            e.printStackTrace();
+        //Toast.makeText(getActivity(), "MOOO "+json.toString(4), Toast.LENGTH_LONG).show();
+        boolean success, host;
+        try{
+            success = json.getBoolean("success");
         }
+        catch (JSONException e){
+            Toast.makeText(getActivity(), "Server communication error", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+            success = false;
+        }
+        if (success){
+            try {
+                host = json.getBoolean("host");
+            }
+            catch (JSONException e){
+                Toast.makeText(getActivity(), "Server communication error", Toast.LENGTH_LONG).show();
+                e.printStackTrace();
+                host = false;
+            }
+            if (host){
+                //Open the host fragment
+            }
+            else {
+                //Open the "waiting for players fragment"
+            }
+            //store player ID
+        }
+        else {
+            //Toast.makeText(getActivity(), "Connection busy, try again", Toast.LENGTH_LONG).show();
+        }
+
     }
 
     @Override
