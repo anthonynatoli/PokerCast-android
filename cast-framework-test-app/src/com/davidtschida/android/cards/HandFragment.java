b39f4820de6d165f678027f3881632f438a57aac
Rod Blagojevich <joekoncel@yahoo.com>
2014-10-17 14:51:20 -0400

    Now it vibrates from 0 to 15 seconds randomly

--- cast-framework-test-app/src/com/davidtschida/android/cards/HandFragment.java
+++ cast-framework-test-app/src/com/davidtschida/android/cards/HandFragment.java
@@ -395,7 +395,8 @@ public class HandFragment extends CastFragment implements OnMessageReceivedListe
                 if (player_id != null && player_id.equals(turnPlayerID)) {
                     //It's my turn!
                     Vibrator vib = (Vibrator) getActivity().getSystemService(Context.VIBRATOR_SERVICE);
-                    vib.vibrate(20000);
+                    double time = Math.random()*15000;
+                    vib.vibrate((int) time);
 // need to be fixed ***********************************************************************************************************
                     /*
                     SharedPreferences.Editor edit = mPrefs.edit();
